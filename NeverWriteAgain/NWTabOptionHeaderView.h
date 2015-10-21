8ea38f13b5076d88bf31bc49db8e6cd1d784b375
Garrett Kim <gkim@isbx.com>
2015-10-20 23:44:28 -0700

    Added Delegate lesson

--- NeverWriteAgain/NWTabOptionHeaderView.h
+++ NeverWriteAgain/NWTabOptionHeaderView.h
@@ -8,6 +8,32 @@
 
 #import <UIKit/UIKit.h>
 
-@interface NWTabOptionHeaderView : UIView
+//declare an enum that represents the tab modes we can be in
+
+typedef enum  {
+    NWTabOptionHeaderViewTabModeA,
+    NWTabOptionHeaderViewTabModeB,
+    NWTabOptionHeaderViewTabModeC,
+} NWTabOptionHeaderViewTabMode;
+
+//declare a delegate (interface) that knows how to listen for NWTabOptionHeaderView events!
+//we are going to forward declare the NWTabOptionHeaderViewDelegate without really saying what methods it provides because we need to use this class in the NWTabOptionHeaderView interface declaration :P
+@protocol  NWTabOptionHeaderViewDelegate;
+
+@interface NWTabOptionHeaderView : UIView {
+
+}
+
+//this NWTabOptionHeaderView will keep track of a delegate or a class to tell about events happening to it. It should implement the NWTabOptionHeaderViewDelegate interface and should be able to handle the methods declared at the bottom of this header
+@property (assign, nonatomic) id<NWTabOptionHeaderViewDelegate> delegate;
+@property (assign, nonatomic) NWTabOptionHeaderViewTabMode tabMode;
+//public methods for this class
+- (void)setTabMode:(NWTabOptionHeaderViewTabMode)mode;
+@end
+//declare the actual fully fleshed out protocol for the delegate
+@protocol  NWTabOptionHeaderViewDelegate <NSObject>
+//say that these methods are optional for any class that uses the NWTabOptionHeaderViewDelegate interface
+@optional
+- (void) tabOptionHeaderView:(NWTabOptionHeaderView *)tabOptionHeaderView didSelectTabMode:(NWTabOptionHeaderViewTabMode)tabMode;
 
 @end
--- NeverWriteAgain/NWTabOptionHeaderView.m
+++ NeverWriteAgain/NWTabOptionHeaderView.m
@@ -8,14 +8,71 @@
 
 #import "NWTabOptionHeaderView.h"
 
+@interface NWTabOptionHeaderView (){
+    //we linked a constraint from interface builder into the code here
+    //we put it in the m file because we don't want other classes to know about it
+    IBOutlet NSLayoutConstraint *leadingSliderViewConstraint;
+    IBOutlet UIButton *optionAButton;
+    IBOutlet UIButton *optionBButton;
+    IBOutlet UIButton *optionCButton;
+}
+
+@end
+
 @implementation NWTabOptionHeaderView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        NSArray* nib = [[NSBundle mainBundle] loadNibNamed:@"NWTabOptionHeaderView" owner:self options:nil];
+        self = [nib objectAtIndex:0];
+    }
+    
+    return self;
 }
-*/
+
+//override the setTabMode function of our tabMode property to do a little extra
+- (void)setTabMode:(NWTabOptionHeaderViewTabMode)mode {
+    _tabMode = mode; //do it this way to prevent us from calling this function again
+    CGFloat sliderViewOffset = 0;
+    switch (self.tabMode) {
+        case NWTabOptionHeaderViewTabModeA:
+            sliderViewOffset = 0;
+            break;
+        case NWTabOptionHeaderViewTabModeB:
+            sliderViewOffset = optionBButton.frame.origin.x;
+            break;
+        case NWTabOptionHeaderViewTabModeC:
+            sliderViewOffset = optionCButton.frame.origin.x;
+            break;
+        default:
+            break;
+    }
+    //animate for half a second
+    [UIView animateWithDuration:0.5f animations:^{
+        [leadingSliderViewConstraint setConstant:sliderViewOffset];
+    }];
+    
+    //if we have a delegate, tell it that we changed our tab mode
+    if (self.delegate != nil && [self.delegate respondsToSelector:@selector(tabOptionHeaderView:didSelectTabMode:)]) {
+        [self.delegate tabOptionHeaderView:self didSelectTabMode:self.tabMode];
+    }
+}
+
+- (IBAction)optionAPressed:(UIButton *)sender {
+    //if someone is listening to us, tell them we switched our mode
+    [self setTabMode:NWTabOptionHeaderViewTabModeA];
+}
+
+- (IBAction)optionBPressed:(id)sender {
+    [self setTabMode: NWTabOptionHeaderViewTabModeB];
+}
+
+- (IBAction)optionCPressed:(id)sender {
+    [self setTabMode:NWTabOptionHeaderViewTabModeC];
+}
+
+
 
 @end
